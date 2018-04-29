4e54a3c1f757bcd0788b23621e927fc0c0f261e2
eric-yoo <ajoije729@gmail.com>
2018-04-29 20:51:40 +0900

    email verification

--- router/main.js
+++ router/main.js
@@ -1,10 +1,21 @@
+var nodemailer=require("nodemailer");
+var smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "prography.apply",
+        pass: "hansol64"
+    }
+});
+var rand,mailOptions,host,link;
+
+
 module.exports = function(app)
 {
     app.get('/', function(req,res) {
-	res.render('index.html')
+    	res.render('index.html')
 	});
     app.get('/about', function(req,res) {
-	res.render('about.html')
+    	res.render('about.html')
 	});
     app.get('/history', function(req, res) {
       res.render('history.html')
@@ -15,6 +26,48 @@ module.exports = function(app)
     app.get('/project', function(req, res) {
       res.render('project.html')
     });
+    app.get('/send',function(req,res){
+        rand=Math.floor((Math.random() * 100) + 54);
+        host=req.get('host');
+        link="http://"+req.get('host')+"/verify?id="+rand;
+        mailOptions={
+            to : req.query.to,
+            subject : "Please confirm your Email account",
+            html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+        }
+        console.log(mailOptions);
+        smtpTransport.sendMail(mailOptions, function(error, response){
+         if(error){
+                console.log(error);
+            res.end("error");
+         }else{
+                console.log("Message sent: " + response.message);
+            res.end("sent");
+             }
+      });
+    });
+    app.get('/verify',function(req,res){
+      console.log(req.protocol+":/"+req.get('host'));
+      if((req.protocol+"://"+req.get('host'))==("http://"+host))
+      {
+          console.log("Domain is matched. Information is from Authentic email");
+          if(req.query.id==rand)
+          {
+              console.log("email is verified");
+              res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+          }
+          else
+          {
+              console.log("email is not verified");
+              res.end("<h1>Bad Request</h1>");
+          }
+      }
+      else
+      {
+          res.end("<h1>Request is from unknown source");
+      }
+    });
+
     app.get('/etc', function(req, res) {
       res.render('etc.html')
     });
