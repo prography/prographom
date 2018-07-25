e3a808d1dce8e10953f6658deb8ecaa97436cd7b
Yeeun <yesung000@naver.com>
2018-07-25 23:47:08 +0900

    admin.ejs, application.ejs, main.js

--- router/main.js
+++ router/main.js
@@ -121,7 +121,7 @@ module.exports = function(app)
 					var body = req.body;
 		 		  var params = [body.date, body.hour, body.minute];
 
-		      client.query(`SELECT Applications.id AS id, name, sex, DATE_FORMAT(birth, \'%y%m%d\'), phone, college, address, Applications.field AS field, q1, q2, q3, q5, q7, q8
+		      client.query(`SELECT Applications.id AS id, name, sex, DATE_FORMAT(birth, \'%y-%m-%d\'), phone, college, address, Applications.field AS field, q1, q2, q3, q5, q7, q8
 		    	FROM Applications, Applicants
 		    	WHERE Applications.id = Applicants.email and interview_date = ? and interview_hour = ? and interview_min = ?;
 				SELECT Q4.field AS q4_field, term, activity, application_id
@@ -131,7 +131,7 @@ module.exports = function(app)
 			 				 console.log(error);
 			 			 }
 			 			 else {
-							 console.log(results);
+							 //console.log(results);
 			 				 res.send(results);
 			 			 }
 					});
@@ -197,19 +197,62 @@ module.exports = function(app)
 
 	// DB에 내용 추가
 	app.post('/application', function(req, res) {
-		var id = req.query.id;
+		//var id = req.query.id;
 		var body = req.body;
+		var id = body.id;
 		var isSubmit = 0;
 		if(body.submit) isSubmit = 1;
-
+		
+		console.log(body);
+		console.log("id = " + id);
+		
 		// record가 있는지 없는지 select문으로 체크
-		client.query(`SELECT * FROM Applications WHERE `),
+		client.query(`SELECT * FROM Applications WHERE id = ?`,["id"], function(error, result){
+			console.log(result);
+			// 해당 id가 db에 존재하지 않으면 모든 내용을 insert
+			if (result.length == 0) {
+				/* client.query(`INSERT INTO Applications (id, sex, birth, college, address, field, q1, q2, q3, q5, q7, q8, submit) VALUES (?,?,?,?,?,?,?,?,?,?,?,?, 0)` ,[
+					id, body.sex, body.birth, body.college, body.address, body.field, body.q1, body.q2, body.q3, body.q5, body.q7, body.q8
+				], function(error, result){ */
+				client.query(`INSERT INTO Applications (id, sex, birth, college, address, field, q1, q2, q3, q5, q7, q8, submit) VALUES (?,?,?,?,?,?,?,?,?,?,?,?, 0)` ,[
+					id, body.sex, body.birth, body.college, body.address, body.field, body.q1, body.q2, body.q3, body.q5, body.q7, body.q8
+				], function(error, result){
+					if (error)
+						console.log("Error!");
+					else {
+						client.query(`INSERT INTO Applicants (name, phone, email, n_th, survived) VALUES (?, ?, ?, 3, 1)`, [body.name, body.phone, id], function(error, result) {
+							// q4 여러개일때 어떻게 넣지?
+							client.query(`INSERT INTO Q4 (field, term, activity, application_id) VALUES (?, ?, ?, ?)`, [body.q4_field, body.term, body.activity, id], function(error, result){
+							});
+						});
+					}
+				});
+			}
+			// 해당 id가 db에 존재하면 update
+			else {
+				client.query(`UPDATE Applications SET sex=?, birth=?, college=?, address=?, field=?, q1=?, q2=?, q3=?, q5=?, q7=?, q8=? WHERE id=?`, [
+					body.sex, body.birth, body.college, body.address, body.field, body.q1, body.q2, body.q3, body.q5, body.q7, body.q8, id
+				], function(error, result) {
+					client.query(`UPDATE Applicants SET name=?, phone=? WHERE email=?`, [body.name, body.phone, id], function(error, result) {
+						// q4 여러개일때 어떻게 넣지?
+						client.query(`UPDATE Q4 SET field=?, term=?, activity=? WHERE application_id = ?`, [body.q4_field, body.term, body.activity, id], function(error, result){
+						});
+					});
+				});
+			}
+			
+			//submit 버튼이면 submit을 1로 update
+			if (isSubmit == 1){
+				client.query(`UPDATE Applications SET submit = 1 WHERE id = ?`, [user], function(){
+				});
+			}
+		});			
 		// 없을때는 insert, complete = isSubmit, 있을때는 update (id빼고 다) => complete == 1이면 Exception (이미 제출되었습니다!), 0이면 update, complete = isSubmit
-		client.query(`INSERT INTO applications (id, sex, college, address, field, q1, q2, q3, q5) VALUES (?,?,?,?,?,?,?,?,?)` [ //
-			id, body.college, body.address, body.field, body.q1, body.q2, body.q3, body.q5
+		/* client.query(`INSERT INTO applications (id, sex, college, address, field, q1, q2, q3, q5) VALUES (?,?,?,?,?,?,?,?,?)` [ //
+			id, baody.college, body.address, body.field, body.q1, body.q2, body.q3, body.q5
 		], function() {
 			res.redirect('/application');
-		});
+		})); */
 	});
 
     app.post('/send',function(req,res){
@@ -285,13 +328,14 @@ module.exports = function(app)
       // 정보가 있으면 select, 없으면 그냥~~
      require('date-utils');
         var id=req.query.id;
-            var user=reverseHash(id);
+        var user=reverseHash(id);
         var answers=['blah1','blah2','blah3','blah4'];
          var newDate = new Date();
           var time = newDate.toFormat('YYYY-MM-DD HH24:MI:SS');
-               var sql = `SELECT name, sex, birth, phone, college, address, field , q1, q2, q3, q5, q7, q8
-               FROM Applications, Applicants
-               WHERE Applications.id = Applicants.email and Applications.id = ?`;
+               
+			   //var user = 'yesung000@naver.com';
+			//var id = '4182a668a2d3924d6e4c5b1cb4d8e0cd213b9bfb4085e57786f2d182e09a74ca';
+
       //find the user info by the id from database
 
 
@@ -307,18 +351,10 @@ module.exports = function(app)
        // console.log(time); // remove
        // apply 진행 중
        if(id in hashMap){
+		if (true) {
 
-          if (time < '2018-08-01 00:00:00') {
-                  client.query(sql, [user], function (error, results) {
-                        if (error){
-                            console.log(error);
-                        }
-                        else {
-                            console.log(results);
-                            res.render('application', {data:results});
-                        }
-                  });
-                }
+          if (time < '2018-08-01 00:00:00')
+             res.render('application', data);
           // after apply
           else if (time < '2018-08-07 00:00:00')
              res.render('recruit-fin', data);
