94e7929792414c4116cb204847bb38769e35383e
Ubuntu <ubuntu@ip-172-31-19-199.ap-northeast-2.compute.internal>
2018-07-26 02:00:30 +0000

    to run on server

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function() {
-  console.log("Express server has started on port 3000")
+var server = app.listen(80, function() {
+  console.log("Express server has started on port 80")
 });
 
 app.use(express.static('public'));
