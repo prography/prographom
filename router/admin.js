ca1c95d680954a4eef9d91169b862b781c0a85db
Yeeun <yesung000@naver.com>
2018-07-28 12:02:57 +0900

    why no post????

--- router/admin.js
+++ router/admin.js
@@ -54,4 +54,38 @@ router.get('/', function(req, res) {
     }
 });
 
+router.post('/', function(req, res) {//조회하기 클릭 시 처리
+			id=req.body.admin_id;
+			pw=req.body.admin_pw;
+			// console.log(id, pw);
+			// if(verify_user(id, pw)){
+				if(req.query.filter=='interviewTime'){
+					var body = req.body;
+		 		  var params = [body.date, body.hour, body.minute];
+				  console.log(params);
+
+		      client.query(`SELECT Applications.id AS id, name, sex, DATE_FORMAT(birth, \'%y-%m-%d\'), phone, college, address, Applications.field AS field, q1, q2, q3, q5, q7, q8
+		    	FROM Applications, Applicants
+		    	WHERE Applications.id = Applicants.email and interview_date = ? and interview_hour = ? and interview_min = ?;
+				SELECT Q4.field AS q4_field, term, activity, application_id
+		    	FROM Applications, Q4
+		    	WHERE Applications.id = Q4.application_id`, params, function (error, results) {
+			 			 if (error){
+			 				 console.log(error);
+			 			 }
+			 			 else {
+							 //console.log(results);
+			 				 res.send(results);
+			 			 }
+					});
+				} else{
+					res.redirect('/admin');
+				}
+
+			// }
+			// else{
+			// 	res.send("<h1>nope, don't even try</h1>")
+
+  	});
+
 module.exports = router;
