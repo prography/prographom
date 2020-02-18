8a54da907e7a247906657cf7bf0ed0618c7d15ee
geusan <dnay2k@gmail.com>
2020-02-18 23:20:25 +0900

    params ㅇㅖ시 추가

--- utils/Mailer.js
+++ utils/Mailer.js
@@ -1,5 +1,18 @@
 const awsSdk = require('aws-sdk');
 
+/**
+ * 
+ * @param {SendRawEmailRequest} params
+ * {
+   Source: "no-reply@prography.org",
+   Destinations: [
+     "destination@gmail.com"
+   ],
+   RawMessage: {
+     Data: '<div>Hello world!</div>'
+   },
+ }
+ */
 async function sendRawEmail(params) {
   const mailer = new awsSdk.SES({
     region: 'us-west-2',
