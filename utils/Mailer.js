85daa3855247ae532fdcb983491aa9ce3ba0bf90
geusan <dnay2k@gmail.com>
2020-02-18 23:38:33 +0900

    Modify method RawEmail to Email

--- utils/Mailer.js
+++ utils/Mailer.js
@@ -3,27 +3,42 @@ const awsSdk = require('aws-sdk');
 /**
  * 
  * @param {SendRawEmailRequest} params
- * {
-   Source: "no-reply@prography.org",
-   Destinations: [
-     "destination@gmail.com"
-   ],
-   RawMessage: {
-     Data: '<div>Hello world!</div>'
-   },
- }
- */
-async function sendRawEmail(params) {
+//    {
+//     Source: "Prography <no-reply@prography.org>",
+//     Destination: {
+//       ToAddresses: [
+//         "destination@gmail.com"
+//       ]
+//     },
+//     Message: {
+//       Subject: {
+//         Charset: "UTF-8",
+//         Data: "[프로그라피] 지원서입니다.",
+//       },
+//       Body: {
+//         Html: {
+//           Charset: "UTF-8",
+//           Data: "<h1>Hello world!</h1>",
+//         },
+//         Text: {
+//           Charset: "UTF-8",
+//           Data: "Hello world!",
+//         },
+//       },
+//     },
+//   }
+*/
+async function sendEmail(params) {
   const mailer = new awsSdk.SES({
     region: 'us-west-2',
     apiVersion: '2010-12-01',
   });
   return new Promise((resolve, reject) => {
-    mailer.sendRawEmail(params, (err, data) => {
+    mailer.sendEmail(params, (err, data) => {
       if (err) reject(err);
       resolve(data);
     });
   });
 }
 
-exports.sendRawEmail = sendRawEmail;
+exports.sendEmail = sendEmail;
